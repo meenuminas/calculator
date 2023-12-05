7c18c101f6d98cd94b4b9ca7a4fff0a680f4151b
minas k s <meenuminas2001@gmail.com>
2023-12-05 12:43:57 +0530

    first comm

---
+++ calc.js
@@ -0,0 +1,14 @@
+function displayData(e){
+    result.value+=e
+}
+function out(){
+    result.value=eval(result.value)
+}
+function Allclear(){
+    result.value=""
+}
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
+
+
